c3101617dd2fb53e17531e9c2dc72ebabd892813
akanshapatil25 <akapatil25@gmail.com>
2025-05-21 15:00:52 +0000

    if-else practice

--- text.js
+++ text.js
@@ -1,1 +1,19 @@
-console.log("akansha");
+const accountEmail = "akansha@gmail.com";
+const accPassword =  "1234";
+
+
+
+const correctEmail = "akansha@gmail.com";
+const correctPassword = "1234";
+
+
+// Check email and password
+if (accountEmail === correctEmail && accPassword === correctPassword) {
+    console.log("Login successful!");
+} else if (accountEmail !== correctEmail && accPassword !== correctPassword) {
+    console.log("Both email and password are incorrect.");
+} else if (accountEmail !== correctEmail) {
+    console.log("Email is incorrect.");
+} else if (accPassword !== correctPassword) {
+    console.log("Password is incorrect.");
+}
